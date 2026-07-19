5032ef4b61901370f9bb3f1914acc8730fb14485
Sushant Gupta <sushantgupta13@outlook.com>
2026-07-19 22:53:36 +1000

    Added GET /bets to list all bets

--- src/main/java/io/sushant/betslip/BetController.java
+++ src/main/java/io/sushant/betslip/BetController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/bets")
@@ -32,5 +33,10 @@ public class BetController {
         kafkaTemplate.send("bets.placed", event);
 
         return savedBet;
+    }
+
+    @GetMapping
+    public java.util.List<Bet> getAllBets() {
+        return betRepository.findAll();
     }
 }
