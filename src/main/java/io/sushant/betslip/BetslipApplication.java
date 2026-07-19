af240f6d6d5e4e5eb480f8d9313f2a1c327f15b3
Sushant Gupta <sushantgupta13@outlook.com>
2026-07-19 16:23:11 +1000

    Initial Commit: SpringBoot + Kafka + Pgres Setup

---
+++ src/main/java/io/sushant/betslip/BetslipApplication.java
@@ -0,0 +1,13 @@
+package io.sushant.betslip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetslipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetslipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/sushant/betslip/BetslipApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sushant.betslip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetslipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
